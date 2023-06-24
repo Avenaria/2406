c49cdf611607f0df8aa83aa053ac62f76bbfdbae
1 <1@LAPTOP-8U2CR0NO>
2023-06-24 12:16:38 +0300

    Добавьте файлы проекта.

---
+++ 2406/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2406
+{
+    using System;
+    using System.IO;
+    using System.Runtime.Remoting.Messaging;
+
+    //class Stack
+    //{
+
+    //    int[] stack;
+    //    int position = 0;
+
+    //    public Stack(int size)
+    //    {
+    //        stack = new int[size];
+    //    }
+
+    //    public void Push(int a)
+    //    {
+    //        stack[position] = a;
+    //        position++;
+    //    }
+    //}
+
+    //class Convert
+    //{
+
+    //    static void Main()
+    //    {
+    //        int x = 22;
+
+    //        Stack stack = new Stack(10);
+    //        while (x > 0)
+    //        {
+    //            stack.Push(x % 2);
+    //            x /= 2;
+    //        }
+    //        foreach (int i in stack)
+
+
+    //        {
+    //            Console.Write(i);
+    //        }
+
+
+    //        Console.ReadKey();
+    //    }
+
+
+
+
+    /// 2
+    /// 
+    //string[] arr = { "ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять" };
+    //Console.WriteLine("путь к файлу : ");
+    //    StreamReader reader = new StreamReader(Console.ReadLine(), Encoding.Default);
+    //string lines = reader.ReadToEnd().Replace('.', '\n');
+    //    for (int i = 0; i< 10; i++)lines= lines.Replace(i.ToString(), arr[i]);
+    //    Console.WriteLine(lines); 
+    //    reader.Close();    
+    //    Console.WriteLine();    
+    //    Console.ReadKey(true);  
+    ///
+
+    //3
+
+    }
+    
+}
